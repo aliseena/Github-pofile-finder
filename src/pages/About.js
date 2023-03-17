4da55ee7682c02360cdcc2149983e46e98ae2f1a
aliseena <aliseena530@yaoo.com>
2023-03-17 14:13:13 +0500

    about page issue resolved

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,9 +9,7 @@ export default function About() {
 
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <Link to="/">
-            <a class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              Go back home
-            </a>
+            <button className="btn-accent">Back to home</button>
           </Link>
         </div>
       </div>
