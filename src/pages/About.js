854e1a93dce7bcf4e5d238c38b83140e5b699466
aliseena <aliseena530@yaoo.com>
2023-03-17 14:07:58 +0500

    Added about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,20 @@
+import { Link } from 'react-router-dom';
 export default function About() {
-  return <div className="home h-screen">About</div>;
+  return (
+    <main class="grid min-h-full place-items-center  py-24 px-6 sm:py-32 lg:px-8">
+      <div class="text-center">
+        <p class="mt-4 text-gray-400 text-3xl">
+          A simple github profile finder with the github rest api
+        </p>
+
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <Link to="/">
+            <a class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              Go back home
+            </a>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
