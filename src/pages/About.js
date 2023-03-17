8c520a5480f542332c72918f28985021fd03e729
aliseena <aliseena530@yaoo.com>
2023-03-17 14:18:25 +0500

    Home button paddings added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { BsArrowLeft } from 'react-icons/bs';
+
 export default function About() {
   return (
     <main class="grid min-h-full place-items-center  py-24 px-6 sm:py-32 lg:px-8">
@@ -8,8 +10,8 @@ export default function About() {
         </p>
 
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <Link to="/">
-            <button className="btn-accent">Back to home</button>
+          <Link to="/" className="btn btn-accent p-3 rounded ">
+            <BsArrowLeft className="mr-2" /> Back to home
           </Link>
         </div>
       </div>
